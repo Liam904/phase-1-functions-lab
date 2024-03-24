6cbb2776339f3a703c1e9414d392c2551d6cce20
Liam <lw433891@gmail.com>
2024-03-24 21:28:23 +0300

    'done'

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+      return blocks - 42;
+    } else {
+      return 42 - blocks;
+    }
+  }
+  
+  
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264; 
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    let blocksTravelled = Math.abs(startBlock - endBlock);
+    return blocksTravelled * 264; 
+  }
+  
+  function calculatesFarePrice(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02; 
+    } else if (distance <= 2500) {
+      return 25; 
+     
+    }
+    else if(distance >2500){
+        return 'cannot travel that far'
+    }
+  }
